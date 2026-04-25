1a3b34476f55c578a73af4c93a7d7b1d25aeaabf
蜡笔小强 <89714521+Asyncload@users.noreply.github.com>
2026-04-25 21:54:53 +0800

    Update XposedModule.kt

--- app/src/main/java/com/lbxq/screen/XposedModule.kt
+++ app/src/main/java/com/lbxq/screen/XposedModule.kt
@@ -5,8 +5,10 @@ import de.robv.android.xposed.XposedBridge
 import de.robv.android.xposed.callbacks.XC_LoadPackage
 
 class XposedModule : IXposedHookLoadPackage {
-    // 不做任何包名判断，只要有应用启动就打印日志
     override fun handleLoadPackage(lpparam: XC_LoadPackage.LoadPackageParam) {
-        XposedBridge.log("✅模块已捕获应用：${lpparam.packageName}🎉")
+        // 检测抖音启动
+        if (lpparam.packageName == "com.ss.android.ugc.aweme") {
+            XposedBridge.log("📱 模块捕获到抖音启动！包名：${lpparam.packageName}")
+        }
     }
 }
